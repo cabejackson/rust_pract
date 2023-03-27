ba249645d16961a199fe7f1ec718cb672ed46081
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-03-27 15:32:26 -0600

    Add start of guessing game: get user input

---
+++ guessing_game/target/debug/deps/guessing_game-6a19897ea54e83a3.d
@@ -0,0 +1,5 @@
+/Users/cabejackson/Documents/projs/learning/rust_pract/guessing_game/target/debug/deps/guessing_game-6a19897ea54e83a3: src/main.rs
+
+/Users/cabejackson/Documents/projs/learning/rust_pract/guessing_game/target/debug/deps/guessing_game-6a19897ea54e83a3.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/cabejackson/Documents/projs/learning/rust_pract/guessing_game/target/debug/guessing_game: /Users/cabejackson/Documents/projs/learning/rust_pract/guessing_game/src/main.rs
