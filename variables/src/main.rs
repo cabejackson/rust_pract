6eb8b7944f5c1254252d7ee5a375542f0802a387
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-03-28 16:18:02 -0600

    Add shadowing example

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -16,5 +16,13 @@ fn main() {
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
     println!("The value of THREE_HOURS_IN_SECONDS is {THREE_HOURS_IN_SECONDS}");
 
-
+    //shadowing ex:
+    let x = 5;
+    let x = x + 1;
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is {x}");
+    }
+   
+    println!("The value of x is {x}");
 }
