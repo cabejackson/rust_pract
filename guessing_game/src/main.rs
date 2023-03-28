12a9c7b7eebb1df55bfb01e0bb59ec710bbfebd3
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-03-28 09:11:04 -0600

    Allow multiple guesses and end game after winning

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -9,22 +9,29 @@ fn main() {
     let secret_num = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is {secret_num}");
 
-    println!("Please input your guess: ");
+    loop {
+        println!("Please input your guess: ");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        
+        println!("You guessed {guess}");
 
-    println!("You guessed {guess}");
-
-    match guess.cmp(&secret_num) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too BIG!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too BIG!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+            
+        }
     }
+    
 
 }
