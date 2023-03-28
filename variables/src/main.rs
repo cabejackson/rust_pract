39ca462ffed72c667bdfc85d76e0d48b60baf04d
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-03-28 16:08:16 -0600

    Add constant example

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -6,9 +6,15 @@ fn main() {
     // x = 6;
     // println!("The value of x is {x}");
 
-    let mut x = 5;
-    println!("The value of x is {x}");
-    x = 6;
-    println!("The value of x is {x}");
+    //mutable example
+    // let mut x = 5;
+    // println!("The value of x is {x}");
+    // x = 6;
+    // println!("The value of x is {x}");
+
+    //constant example
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("The value of THREE_HOURS_IN_SECONDS is {THREE_HOURS_IN_SECONDS}");
+
 
 }
