d493b57af0d919f5d4af6843bc0c3de9f1c26bcc
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-03-28 18:44:00 -0600

    Add parameters example

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,23 @@
 fn main() {
     println!("Hello, world!");
     
-    another_function();
+    // another_function(); // uncomment w/ basic ex
+    // another_function(5); // uncomment w/ params ex 1
+    
+    print_labeled_measurement(5, 'h'); // uncomment w/ params ex 2
+}
+// initial basic example
+// fn another_function() {
+//     println!("Another function!");
+// }
+
+//parameters example
+// fn another_function(x:i32) {
+//     println!("The value of x is {x}");
+// }
+
+//params ex 2
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is {value}{unit_label}");
 }
 
-fn another_function() {
-    println!("Another function!");
-}
