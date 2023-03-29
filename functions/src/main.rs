bea8efdc9104e000711b65db70d9e5ecac757d19
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-03-29 09:51:47 -0600

    Statements vs expressions and comments

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,11 +1,36 @@
 fn main() {
     println!("Hello, world!");
+
+    // statements vs expressions ------------------------------
+    // //this is the statement
+    // let y = {
+    //     //these 2 lines are the expression
+    //     let x = 3;
+    //     x + 1 // note expression doesn't need a semi colon
+    // };
+
+    // println!("The value of y is {y}!");
+    //----------------------------------------------------------
     
+    
+    //--------------------------------------------------
     // another_function(); // uncomment w/ basic ex
     // another_function(5); // uncomment w/ params ex 1
+    //--------------------------------------------------
+
+
+    //-----------------------------------------------------------------
+    // print_labeled_measurement(5, 'h'); // uncomment w/ params ex 2
+    //-----------------------------------------------------------------
     
-    print_labeled_measurement(5, 'h'); // uncomment w/ params ex 2
+    //-------------------------------------
+    // function w/ returns ex 1
+    // let x = five();
+    // println!("The value of x is {x}!");
+    //-------------------------------------
+    
 }
+
 // initial basic example
 // fn another_function() {
 //     println!("Another function!");
@@ -17,7 +42,12 @@ fn main() {
 // }
 
 //params ex 2
-fn print_labeled_measurement(value: i32, unit_label: char) {
-    println!("The measurement is {value}{unit_label}");
-}
+// fn print_labeled_measurement(value: i32, unit_label: char) {
+//     println!("The measurement is {value}{unit_label}");
+// }
+
+//function w/ returns example
+// fn five() -> i32 {
+//     5 // note: no let, fn calls, macros or return...just 5
+// }
 
