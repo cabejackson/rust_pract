7534230059ce68a019e74b92df54ca3afa2c4314
Caleb Jackson <cabejackson@Calebs-MBP.attlocal.net>
2023-03-28 18:40:43 -0600

    Add functions cargo w/ basic functions

---
+++ functions/target/debug/deps/functions-db45776009db6029.d
@@ -0,0 +1,5 @@
+/Users/cabejackson/Documents/projs/learning/rust_pract/functions/target/debug/deps/functions-db45776009db6029: src/main.rs
+
+/Users/cabejackson/Documents/projs/learning/rust_pract/functions/target/debug/deps/functions-db45776009db6029.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/Users/cabejackson/Documents/projs/learning/rust_pract/functions/target/debug/functions: /Users/cabejackson/Documents/projs/learning/rust_pract/functions/src/main.rs
